2ab6a8a765c25a06d1ec97ad7667ae94a3520042
facuadrado <55325783+facuadrado@users.noreply.github.com>
2020-07-26 13:44:02 -0400

    Add files via upload

--- DisplayCircuit.py
+++ DisplayCircuit.py
@@ -5,7 +5,8 @@ Created on Sun Jul 26 00:47:08 2020
 @author: facua
 """
 # import required classes
-
+import os
+import shutil
 from PIL import Image, ImageDraw, ImageFont
 
 def DrawText(draw, text, color, font, x, y):
@@ -37,7 +38,13 @@ def DrawCircuit(filterType, z0='N/A', l='N/A', c='N/A', ls='N/A', lx='N/A', cs='
         DrawText(draw, z0, color, font, 469, 490)
         DrawText(draw, z0, color, font, 1024, 490)
         
-        image.save('lowpassfilter.png')
+        try:
+            shutil.rmtree('output')
+            os.mkdir('output')
+        except: 
+            os.mkdir('output')
+        image.save('output/lowpassfilter.png')
+        print('Filter schematic saved to Output folder.')
         image.show()
         
     elif filterType == 'highpass':
@@ -57,7 +64,13 @@ def DrawCircuit(filterType, z0='N/A', l='N/A', c='N/A', ls='N/A', lx='N/A', cs='
         DrawText(draw, z0, color, font, 469, 490)
         DrawText(draw, z0, color, font, 1024, 490)
         
-        image.save('highpassfilter.png')
+        try:
+            shutil.rmtree('output')
+            os.mkdir('output')
+        except: 
+            os.mkdir('output')
+        image.save('output/highpassfilter.png')
+        print('Filter schematic saved to Output folder.')
         image.show()
         
     elif filterType == 'bandpass':
@@ -89,36 +102,48 @@ def DrawCircuit(filterType, z0='N/A', l='N/A', c='N/A', ls='N/A', lx='N/A', cs='
         DrawText(draw, z0, color, font, 489, 556)
         DrawText(draw, z0, color, font, 1066, 556)
         
-        image.save('bandpassfilter.png')
+        try:
+            shutil.rmtree('output')
+            os.mkdir('output')
+        except: 
+            os.mkdir('output')
+        image.save('output/bandpassfilter.png')
+        print('Filter schematic saved to Output folder.')
         image.show()
     else:
         # Draw Series Conductor Values
-        DrawText(draw, cs, color, font, 638, 112)
-        DrawText(draw, cs, color, font, 914, 112)
-        DrawText(draw, cs, color, font, 334, 423)
-        DrawText(draw, cs, color, font, 1090, 423)
+        DrawText(draw, cs, color, font, 256, 186)
+        DrawText(draw, cs, color, font, 900, 186)
+        DrawText(draw, cs, color, font, 256, 612)
+        DrawText(draw, cs, color, font, 900, 612)
 
         # Draw Parallel Conductor Values
-        DrawText(draw, cx, color, font, 220, 245)
-        DrawText(draw, cx, color, font, 796, 245)
-        DrawText(draw, cx, color, font, 220, 690)
-        DrawText(draw, cx, color, font, 796, 690)
+        DrawText(draw, cx, color, font, 203, 495)
+        DrawText(draw, cx, color, font, 995, 495)
+        DrawText(draw, cx, color, font, 517, 240)
+        DrawText(draw, cx, color, font, 682, 240)
         
         # Draw Series Inductors Values
-        DrawText(draw, ls, color, font, 480, 112)
-        DrawText(draw, ls, color, font, 770, 112)
-        DrawText(draw, ls, color, font, 134, 423)
-        DrawText(draw, ls, color, font, 1245, 423)
+        DrawText(draw, ls, color, font, 425, 317)
+        DrawText(draw, ls, color, font, 720, 317)
+        DrawText(draw, ls, color, font, 425, 720)
+        DrawText(draw, ls, color, font, 720, 720)
         
         # Draw Parallel Inductors Values
-        DrawText(draw, lx, color, font, 578, 266)
-        DrawText(draw, lx, color, font, 1156, 266)
-        DrawText(draw, lx, color, font, 578, 712)
-        DrawText(draw, lx, color, font, 1156, 712)
+        DrawText(draw, lx, color, font, 405, 22)
+        DrawText(draw, lx, color, font, 775, 22)
+        DrawText(draw, lx, color, font, 184, 261)
+        DrawText(draw, lx, color, font, 978, 261)
         
         # Draw Resistance Values
-        DrawText(draw, z0, color, font, 489, 556)
-        DrawText(draw, z0, color, font, 1066, 556)
+        DrawText(draw, z0, color, font, 425, 500)
+        DrawText(draw, z0, color, font, 900, 500)
         
-        image.save('bandstopfilter.png')
+        try:
+            shutil.rmtree('output')
+            os.mkdir('output')
+        except: 
+            os.mkdir('output')
+        image.save('output/bandstopfilter.png')
+        print('Filter schematic saved to Output folder.')
         image.show()
